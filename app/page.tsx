42a95584659b297245b12017075b6319f7fca93a
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-16 12:49:01 -0600

    Actualizacion TailWind 5

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <a href="#">Acerca</a>
         </nav>
         <div className={styles.center}>
-          <h1 className="text-4xl font-bold">Soy Felipe</h1>
+          <h1 className="text-4xl font-bold mb-8">Soy Felipe</h1>
           <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
             Iniciar Sesión
           </button>
