fbb4c5b4eea9ce54b13eae090afdc3eafd2c5553
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-16 12:35:04 -0600

    Actualizacion TailWind 2

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,13 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex flex-col justify-center items-center h-screen">
-      <nav className="bg-gray-800 text-white w-full p-4">
-        <div className="flex justify-between items-center max-w-4xl mx-auto">
+      <nav className="bg-gray-800 text-white w-full p-4 flex justify-end">
+        <div className="max-w-4xl">
           <Link href="/">
             <a className="text-xl font-bold">Home</a>
           </Link>
           <Link href="/about">
-            <a className="text-xl">Acerca</a>
+            <a className="text-xl ml-4">Acerca</a>
           </Link>
         </div>
       </nav>
