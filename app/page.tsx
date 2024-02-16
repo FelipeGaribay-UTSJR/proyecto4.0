b6c38134f83c2c8b02fff5386467d5beb478ebeb
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-16 12:38:43 -0600

    Actualizacion TailWind 3

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,16 @@
 import styles from "@/styles/index.module.css";
-import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-center items-center h-screen">
-      <nav className="bg-gray-800 text-white w-full p-4 flex justify-end">
-        <div className="max-w-4xl">
-          <Link href="/">
-            <a className="text-xl font-bold">Home</a>
-          </Link>
-          <Link href="/about">
-            <a className="text-xl ml-4">Acerca</a>
-          </Link>
+    <main>
+      <div className={styles.container}>
+        <nav className={styles.navbar}>
+          <a href="#">Home</a>
+          <a href="#">Acerca</a>
+        </nav>
+        <div className={styles.center}>
+          <h1 className="text-4xl font-bold">Soy Felipe</h1>
         </div>
-      </nav>
-      <div className="mt-8">
-        <h1 className="text-4xl font-bold text-center">Tu Nombre</h1>
       </div>
     </main>
   );
