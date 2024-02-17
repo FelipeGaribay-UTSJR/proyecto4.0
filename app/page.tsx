d9e288447b751d3a373bbdd0dedbccd27c692381
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-16 20:50:32 -0600

    No se actuliza page :/

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,7 @@ export default function Home() {
         </nav>
         <div className={styles.center}>
           <h1 className="text-4xl font-bold mb-8">Soy Felipe</h1>
+          <h2>TI03SM-22</h2>
         </div>
       </div>
     </main>
