11618ee0983a0f407371066dcc857c04433de03a
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-16 20:53:44 -0600

    Ya se actualizo

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
           <a href="#">Acerca</a>
         </nav>
         <div className={styles.center}>
-          <h1 className="text-4xl font-bold mb-8">Soy Felipe</h1>
+          <h1 className="text-4xl font-bold mb-8">Soy Felipe </h1>
           <h2>TI03SM-22</h2>
         </div>
       </div>
