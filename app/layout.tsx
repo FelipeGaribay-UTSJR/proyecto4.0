2a978ecc4aca52782dd9d7f971b7651577bad848
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-02-20 14:47:10 -0600

    Navigation

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Navigation from "./components/Navigation";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,6 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+          <Navigation/>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
 import styles from "@/styles/index.module.css";
+import Navigation from "./components/Navigation";
 
 export default function Home() {
   return (
     <main>
-      <div className={styles.container}>
-        <nav className={styles.navbar}>
-          <a href="#">Home</a>
-          <a href="#">Acerca</a>
-        </nav>
+     {/* <Navigation/>*/}
         <div className={styles.center}>
-          <h1 className="text-4xl font-bold mb-8">Soy Felipe </h1>
+          <h1 className="text-4xl font-bold mb-8">Soy Felipe, </h1>
           <h2>TI03SM-22</h2>
         </div>
-      </div>
     </main>
   );
 }
