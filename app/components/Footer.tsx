2be27c5629948c8d22b751b9335d2d4aaa88851e
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-03-12 17:40:08 -0600

    Otra raya mas al tigre

---
+++ app/components/Footer.tsx
@@ -0,0 +1,12 @@
+
+import styles from "@/styles/index.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>© Universidad tecnologica de San Juan del Río. Todos los derechos reservados.</p>
+    </footer>
+  );
+};
+
+export default Footer;
