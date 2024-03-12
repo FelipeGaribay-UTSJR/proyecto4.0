33a18abd76b7cdafaf7010eb5a63b40d9959c754
FelipeGaribay-UTSJR <felipegu.ti22@utsjr.edu.mx>
2024-03-12 17:52:14 -0600

    Otra raya mas al tigre 2

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,16 +1,23 @@
 // Home.tsx
 import React from 'react';
-import Image from "next/image";
+import Image from 'next/image';
 
-export default function Home() {
+const Home: React.FC = () => {
   return (
     <main>
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold mb-8">Soy Felipe</h1>
-        <h2>TI03SM-22</h2>
-        <img src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/11/last-us-parte-ii-arte-conceptual_1.jpg?tf=1200x" alt="The last of us" />
-        <p>The last of us forest</p>
+        <h1 className="text-4xl font-bold mb-8 text-center">Soy Felipe</h1>
+        <h2 className="text-center">TI03SM-22</h2>
+        <Image
+          src="https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2019/11/last-us-parte-ii-arte-conceptual_1.jpg?tf=1200x"
+          alt="The Last of Us"
+          width={1200}
+          height={800}
+        />
+        <p className="text-center">The Last of Us - Bosque</p>
       </div>
     </main>
   );
-}
+};
+
+export default Home;
